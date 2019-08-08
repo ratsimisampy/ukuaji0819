5b2270572e7fe1ac9869547528afdc86adec5ba4
ratsimisampy <ratsimisampy@yahoo.fr>
2019-08-08 21:10:14 +0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get "home/vision", to: "home#vision", as: "home/vision"
+  get "home/action", to: "home#action", as: "home/action"
+  get "home/help", to: "home#help", as: "home/help"
+end
